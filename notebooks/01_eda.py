42c82e81933a0af3315db40da48674285c6e4689
Nick <babukhnikita@gmail.com>
2026-05-23 20:43:54 +0200

    add: target distribution analysis

--- notebooks/01_eda.py
+++ notebooks/01_eda.py
@@ -15,8 +15,37 @@ def _():
 @app.cell
 def _(pd):
     high_pop = pd.read_csv("../data/raw/spotify/high_popularity_spotify_data.csv")
-    low_pop = pd.read_csv("../data/raw/spotify/low_popularity_spotify_data.csv")
     return (high_pop,)
+
+
+@app.cell
+def _(high_pop):
+    high_pop.describe()
+    return
+
+
+@app.cell
+def _(high_pop):
+    high_pop.shape
+    return
+
+
+@app.cell
+def _(high_pop):
+    high_pop.info()
+    return
+
+
+@app.cell
+def _(high_pop):
+    high_pop.isna().sum()
+    return
+
+
+@app.cell
+def _(high_pop):
+    high_pop.duplicated().sum()
+    return
 
 
 @app.cell(hide_code=True)
@@ -30,15 +59,14 @@ def _(mo):
 @app.cell(hide_code=True)
 def _(mo):
     mo.md(r"""
-    By executing command below, we see, that the most popular genres are rock, pop and hip-hop with significant difference from other genres below them.
+    By executing command below, we see, that the most popular genre is `pop` with significant difference from other genres.
     """)
     return
 
 
 @app.cell
 def _(high_pop):
-    # this line may be underlined as an error, but it's just static-analysis noise (warning).
-    high_pop.groupby(by='playlist_genre').size().sort_values(ascending=False).head(5)  # ty:ignore[no-matching-overload]
+    high_pop.groupby(by='playlist_genre').size().sort_values(ascending=False).head(5) #ty:ignore[no-matching-overload]
     return
 
 
@@ -58,21 +86,57 @@ def _():
 
 @app.cell
 def _(high_pop, target_genres):
-    result = high_pop[high_pop['playlist_genre'].isin(target_genres)].groupby(by='playlist_genre').agg({
+    top_three = high_pop[high_pop['playlist_genre'].isin(target_genres)].groupby(by='playlist_genre').agg({
+        'track_popularity':'mean',
         'energy':'mean',
         'danceability':'mean',
         'playlist_genre':'count',
         'loudness':'mean',
         'liveness':'mean',
         'valence':'mean',
-        'speechiness':'mean'
+        'tempo':'mean',
+        'duration_ms':'mean'
     })
-    return (result,)
+
+    top_three
+    return
+
+
+@app.cell(hide_code=True)
+def _(mo):
+    mo.md(r"""
+    The most popular music must be energetic, danceable, not loud, can be perfomed live, lasts about 2 minutes and it's happy.
+    """)
+    return
+
+
+@app.cell(hide_code=True)
+def _(mo):
+    mo.md(r"""
+    ---
+    """)
+    return
 
 
 @app.cell
-def _(result):
-    result
+def _(high_pop):
+    high_pop["track_popularity"].hist(bins=30)
+    return
+
+
+@app.cell(hide_code=True)
+def _(mo):
+    mo.md(r"""
+    `Left-skewed distribution` - most of the songs popularity parameter is between 65-75
+    """)
+    return
+
+
+@app.cell(hide_code=True)
+def _(mo):
+    mo.md(r"""
+    ---
+    """)
     return
 
 
