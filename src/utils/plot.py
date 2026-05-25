06a2bd390ef09b437f4110483f601c4f70296418
Nick <babukhnikita@gmail.com>
2026-05-25 11:39:17 +0200

    fix: now one function shows two plots

--- src/utils/plot.py
+++ src/utils/plot.py
@@ -2,9 +2,8 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 
 def show_plot_4x4(scatter: bool, box: bool, y, x1, x2, x3, x4):
-    fig, axes = plt.subplots(2, 2, figsize=(10, 8))
-
     if(scatter):
+        fig, axes = plt.subplots(2, 2, figsize=(10, 8))
         sns.scatterplot(x=x1, y=y, ax=axes[0, 0])
         sns.scatterplot(x=x2, y=y, ax=axes[0, 1])
         sns.scatterplot(x=x3, y=y, ax=axes[1, 0])
@@ -19,10 +18,11 @@ def show_plot_4x4(scatter: bool, box: bool, y, x1, x2, x3, x4):
         plt.show()
 
     if(box):
-        sns.boxplot(x=x1, y=y, ax=axes[0, 0])
-        sns.boxplot(x=x2, y=y, ax=axes[0, 1])
-        sns.boxplot(x=x3, y=y, ax=axes[1, 0])
-        sns.boxplot(x=x4, y=y, ax=axes[1, 1])
+        fig, axes = plt.subplots(2, 2, figsize=(10, 8))
+        sns.boxplot(x=x1, ax=axes[0, 0])
+        sns.boxplot(x=x2, ax=axes[0, 1])
+        sns.boxplot(x=x3, ax=axes[1, 0])
+        sns.boxplot(x=x4, ax=axes[1, 1])
 
         axes[0, 0].set_title("Energy vs Popularity")
         axes[0, 1].set_title("Danceability vs Popularity")
