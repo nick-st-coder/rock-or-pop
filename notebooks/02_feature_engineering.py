e3e89f6af16c5ac6fed67af11a770b8bba5f2ef6
Nick <babukhnikita@gmail.com>
2026-05-30 19:49:11 +0200

    modified: outliers removed

--- notebooks/02_feature_engineering.py
+++ notebooks/02_feature_engineering.py
@@ -9,6 +9,8 @@ def _():
     import pandas as pd
     import numpy as np
 
+    from sklearn.preprocessing import OneHotEncoder, StandardScaler
+
     high_pop = pd.read_csv('../data/raw/spotify/high_popularity_spotify_data.csv')
     low_pop = pd.read_csv('../data/raw/spotify/low_popularity_spotify_data.csv')
     return high_pop, low_pop, pd
@@ -50,19 +52,20 @@ def _(df, pd):
     return
 
 
-@app.cell(hide_code=True)
-def _(mo):
-    mo.md(r"""
-    ---
-    """)
-    return
-
-
 @app.cell
 def _(df):
-    clean_df = df.drop(columns=['track_artist', 'track_href', 'uri', 'track_album_name', 'playlist_name', 'track_id',
-    'track_name', 'track_album_id', 'id', 'type', 'playlist_id', 'analysis_url', 'time_signature', 'track_album_release_date'])
-    return (clean_df,)
+    Q1 = df['duration_ms'].quantile(0.25)
+    Q3 = df['duration_ms'].quantile(0.75)
+
+    IQR = Q3 - Q1
+
+    lower = Q1 - 1.5 * IQR
+    upper = Q3 + 1.5 * IQR
+
+    df_no_outliers = df[
+        (df['duration_ms'] >= lower) & (df['duration_ms'] <= upper)
+    ]
+    return (df_no_outliers,)
 
 
 @app.cell(hide_code=True)
@@ -74,28 +77,45 @@ def _(mo):
 
 
 @app.cell
-def _(clean_df, df):
-    clean_df['year_of_release'] = df['track_album_release_date'].dt.year
-    clean_df['month_of_release'] = df['track_album_release_date'].dt.month
+def _(df_no_outliers):
+    df_clean = df_no_outliers.drop(columns=['track_artist', 'track_href', 'uri', 'track_album_name', 'playlist_name', 'track_id',
+    'track_name', 'track_album_id', 'id', 'type', 'playlist_id', 'analysis_url', 'time_signature', 'track_album_release_date'])
+    return (df_clean,)
+
+
+@app.cell(hide_code=True)
+def _(mo):
+    mo.md(r"""
+    ---
+    """)
     return
 
 
 @app.cell
-def _(clean_df):
-    clean_df['is_popular'] = clean_df['track_popularity'] > 68
+def _(df_clean, df_no_outliers):
+    df_clean['year_of_release'] = df_no_outliers['track_album_release_date'].dt.year
+    df_clean['month_of_release'] = df_no_outliers['track_album_release_date'].dt.month
     return
 
 
 @app.cell
-def _(clean_df):
+def _(df_clean):
     popular_genres = ['rock', 'pop', 'hip-hop']
-    clean_df['is_popular_genre'] = clean_df['playlist_genre'].isin(popular_genres)
+    df_clean['is_popular_genre'] = df_clean['playlist_genre'].isin(popular_genres)
     return
 
 
 @app.cell
-def _(clean_df):
-    clean_df
+def _(df_clean):
+    df_clean
+    return
+
+
+@app.cell(hide_code=True)
+def _(mo):
+    mo.md(r"""
+    ---
+    """)
     return
 
 
